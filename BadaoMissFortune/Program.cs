da1c962f2fd138aacb70cee62073648f392b3afb
fineatall <ahri26120@yahoo.com>
2016-01-09 21:05:39 +0700

    MF standalone

---
+++ BadaoMissFortune/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+namespace BadaoKingdom
+{
+    static class Program
+    {
+        public static readonly List<string> SupportedChampion = new List<string>()
+        {
+            "MissFortune"
+        };
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            if (!SupportedChampion.Contains(ObjectManager.Player.ChampionName))
+            {
+                return;
+            }
+            Game.PrintChat("<font color=\"#24ff24\">Badao </font>" + "<font color=\"#ff8d1a\">" + 
+                ObjectManager.Player.ChampionName + "</font>" + "<font color=\"#24ff24\"> loaded!</font>");
+            BadaoChampion.BadaoMissFortune.BadaoMissFortune.BadaoActivate();
+        }
+    }
+}
